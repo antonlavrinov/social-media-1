46d41e0e8c695950115250eb228d70d8e6dd51fe
antonlavrinov <alv.anton.lavrinov@gmail.com>
2021-06-26 10:13:06 +0300

    sd

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ const port = process.env.PORT || 5000;
 //   "mongodb+srv://anton:OZoNamhYQYBZ7f6v@cluster0.ifqtj.mongodb.net/app?retryWrites=true&w=majority";
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path_join(__dirname, "../client/build")));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
